7611c1fcfd038b8d4b608c1b0cb7b63dd9e33594
GANIDU2413 <ganidusahan@gmail.com>
2024-05-07 10:53:25 +0530

    Update ChatController.java

--- src/main/java/com/chandimal/auctionApp/Controller/ChatController.java
+++ src/main/java/com/chandimal/auctionApp/Controller/ChatController.java
@@ -1,6 +1,8 @@
 package com.chandimal.auctionApp.Controller;
 
 import com.chandimal.auctionApp.DTO.Message;
+import com.chandimal.auctionApp.dao.ChatRoom;
+import com.chandimal.auctionApp.dao.Observer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -8,14 +10,41 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class ChatController {
     @Autowired
     public SimpMessagingTemplate simpMessagingTemplate;
 
+    @Autowired
+    public ChatController() {
+        addObserver(new ChatRoom());
+    }
+
+    private List<Observer> observers = new ArrayList<>();
+
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    private void notifyObservers(Message message) {
+        for (Observer observer : observers) {
+            observer.update(message);
+        }
+    }
+
     @MessageMapping("/message")
     @SendTo("/chatroom/public")
-    public Message receiveMessage(@Payload Message message) {return message;}
+    public Message receiveMessage(@Payload Message message) {
+        notifyObservers(message);
+        return message;
+    }
 
     @MessageMapping("/private-message")
     public Message recMessage(@Payload Message message){
